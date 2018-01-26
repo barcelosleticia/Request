55d7b5016c5429f7cb340cd676e76c4d536f6b08
barcelosleticia <leticia.fbarcelos@outlook.com>
2018-01-26 15:35:56 -0200

    Add files via upload

---
+++ Requisicao/api/Controllers/RequisicaoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.IO;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class RequisicaoController : Controller
+    {
+        public class Requisicao
+        {
+            public string id {get; set;}
+            public string name {get; set;}
+        }
+    }
+}
---
+++ Requisicao/api/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var request = WebRequest.CreateHttp("https://dev.people.com.ai/bug/api/v3/products");
+            request.Method = "GET";
+            
+            using (var takeRequest = request.GetResponse())
+            {
+                var information = takeRequest.GetResponseStream();
+                StreamReader reader = new StreamReader(information);
+                object objRequest = reader.ReadToEnd();
+                Console.WriteLine(objRequest.ToString() );  
+                Console.ReadLine();
+                information.Close();
+                takeRequest.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
